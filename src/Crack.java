c4b52fc7df44e6cbb570c1278dee0d1c79174207
Brandin Crockett <brandincrockett@gmail.com>
2023-03-31 11:53:45 -0700

    Completed Lab 009

--- src/Crack.java
+++ src/Crack.java
@@ -10,6 +10,21 @@ import java.nio.file.Path;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
+class User {
+    private String username;
+    private String passHash;
+
+    public User(String username, String passHash) {
+        this.username = username;
+        this.passHash = passHash;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassHash() {
+        return passHash;
+    }
+}
 public class Crack {
     private final User[] users;
     private final String dictionary;
@@ -20,6 +35,19 @@ public class Crack {
     }
 
     public void crack() throws FileNotFoundException {
+        try (Scanner scanner = new Scanner(new FileInputStream(dictionary))) {
+            while (scanner.hasNextLine()) {
+                String word = scanner.nextLine();
+                for (User user : users) {
+                    if (user.getPassHash().contains("$")) {
+                        String hash = Crypt.crypt(word, user.getPassHash());
+                        if (hash.equals(user.getPassHash())) {
+                            System.out.println("Found password " + word + " for user " + user.getUsername());
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public static int getLineCount(String path) {
@@ -31,6 +59,21 @@ public class Crack {
     }
 
     public static User[] parseShadow(String shadowFile) throws FileNotFoundException {
+        int lineCount = getLineCount(shadowFile);
+        User[] users = new User[lineCount];
+
+        try (Scanner scanner = new Scanner(new FileInputStream(shadowFile))) {
+            int index = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(":");
+                if (parts.length >= 2) {
+                    User user = new User(parts[0], parts[1]);
+                    users[index++] = user;
+                }
+            }
+        }
+        return users;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
